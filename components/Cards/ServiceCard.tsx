dde1a5cce7143d592e510b7441d1de35f93c110a
Andre Rodrigo <andre.rodrigues@probely.com>
2025-05-01 23:57:11 +0100

    feat: improve service cards

--- components/Cards/ServiceCard.tsx
+++ components/Cards/ServiceCard.tsx
@@ -17,13 +17,15 @@ const ServiceCard: React.FC<ServiceCardProps> = ({
 }) => {
   return (
     <div
-      className={`group-service-card rounded-lg border border-[var(--color-border-primary)] p-8 text-left shadow-md transition-all duration-300 ${className}`}
+      className={`group-service-card flex h-full flex-col justify-between rounded-lg border border-[var(--color-border-primary)] p-8 text-left shadow-md transition-all duration-300 ${className}`}
     >
-      <span className="text-sm font-medium text-[var(--color-accent)]">
-        {subtitle}
-      </span>
-      <h3 className="mt-7 text-xl text-[var(--color-primary)]">{title}</h3>
-      <p className="mt-2 text-sm">{description}</p>
+      <div>
+        <span className="text-sm font-light text-[var(--color-accent)]">
+          {subtitle}
+        </span>
+        <h3 className="mt-3 text-xl text-[var(--color-primary)]">{title}</h3>
+        <p className="mt-2 text-sm">{description}</p>
+      </div>
       {!!action && <div className="mt-7">{action}</div>}
 
       {/* Warm, cozy glow effect */}
@@ -31,10 +33,10 @@ const ServiceCard: React.FC<ServiceCardProps> = ({
         <style jsx>{`
           :global(.group-service-card:hover) {
             border-color: transparent;
-            background-color: rgba(255, 255, 255, 0.3); /* branco translúcido */
-            box-shadow: 0 0 50px 4px rgba(255, 255, 255, 0.1),
-              0 0 10px 6px rgba(250, 250, 250, 0.05),
-              0 0 20px 8px rgba(245, 245, 245, 0.03);
+            background-color: rgba(255, 255, 255, 0.6);
+            box-shadow: 0 0 10px 4px rgba(255, 255, 255, 0.05),
+              0 0 20px 6px rgba(255, 255, 255, 0.03),
+              0 0 30px 8px rgba(255, 255, 255, 0.01);
           }
         `}</style>
       )}
