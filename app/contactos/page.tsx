818e199f0daf68727444a6111f3d9dc09a34e25b
Andre Rodrigo <andre.rodrigues@probely.com>
2025-05-01 21:26:10 +0100

    feat: add husky

--- app/contactos/page.tsx
+++ app/contactos/page.tsx
@@ -17,7 +17,7 @@ function Contacts() {
         src="https://images.unsplash.com/photo-1466781783364-36c955e42a7f?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         title="Contactos"
       />
-      
+
       <Section>
         <motion.div
           variants={containerVariant}
