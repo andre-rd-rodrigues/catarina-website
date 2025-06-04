b9696bd501bb1713ccb6f14298a35a76e0e095b6
andre-rd-rodrigues <andreptrodrigo@gmail.com>
2025-06-04 23:30:27 +0100

    refactor: improve meta text

--- constants/metadata.ts
+++ constants/metadata.ts
@@ -1,33 +1,40 @@
 export const PAGES_METADATA = {
   HOME: {
-    title: 'Dra. Catarina Paixão ⦁ Medicina Integrativa',
+    title: 'Dra. Catarina Paixão ⦁ Medicina Funcional Integrativa',
     description:
       'Dra. Catarina Paixão - Medicina com alma. Une ciência, mente e espiritualidade num cuidado profundo e humano.',
   },
   ABOUT: {
     title: 'Sobre ⦁ Dra. Catarina Paixão',
-    description: 'Sobre',
+    description:
+      'Conheça o meu percurso e como cheguei a integrar diferentes abordagens no cuidado à saúde física, emocional e espiritual.',
   },
   CONTACT: {
     title: 'Contatos ⦁ Dra. Catarina Paixão',
-    description: 'Contatos',
+    description:
+      'Entre em contacto para esclarecer dúvidas ou marcar uma consulta. Vamos juntos trilhar este caminho rumo a uma vida mais saudável e harmoniosa.',
   },
   SERVICES: {
     title: 'Serviços ⦁ Dra. Catarina Paixão',
-    description: 'Serviços',
+    description:
+      'Descubra como posso ajudar através de consultas médicas, sessões de terapia e leituras terapêuticas personalizadas.',
   },
   PRIVACY_POLICY: {
     title: 'Política de Privacidade ⦁ Dra. Catarina Paixão',
-    description: 'Política de Privacidade',
+    description:
+      'Conheça como os seus dados estão protegidos e de que forma utilizo ferramentas de análise com total respeito pela sua privacidade.',
   },
   TERMS_AND_CONDITIONS: {
     title: 'Termos e Condições ⦁ Dra. Catarina Paixão',
-    description: 'Termos e Condições',
+    description:
+      'Saiba em que termos pode navegar neste site, com transparência sobre direitos, responsabilidades e análise de utilização.',
   },
   FAQS: {
     title: 'FAQs ⦁ Perguntas Frequentes',
-    description: 'Perguntas Frequentes',
+    description:
+      'Encontre respostas para as perguntas mais comuns sobre medicina integrativa e os meus serviços.',
   },
+
   /*   BLOG: {
     title: 'Blog',
     description: 'Blog',
